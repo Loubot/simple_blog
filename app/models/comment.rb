11678cf2bffd1ae26e76cdd2f395b42c91943f02
Loubot <louisangelini@gmail.com>
2014-04-17 22:40:31 +0100

    Add comment count to posts view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -16,17 +16,20 @@ class Comment < ActiveRecord::Base
   attr_accessible :title, :body, :author, :content, :status, :post_id
   belongs_to :post
 
-  #before_save :check_count
+  before_save :check_count
 
   after_save :update_count
 
   def check_count
   	p = Post.find(self.post_id)
+    puts "id #{p.id}"
   	puts "old size #{p.comments.size}"
   end
 
   def update_count
   	p = Post.find(self.post_id)  	
-  	p.update_attribute(:comments_count, p.comments.size)  	
+  	p.update_attributes(comments_count: p.comments.count)
+    p.save  
+    puts "new size #{p.comments.size}"	
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 # User.create(username: 'loubot', hashed_password: 'pass', first_name: 'Louis', last_name: 'Angelini', email:'lllouis@yahoo.com', display_name:'loubot', user_level: 9)
 # Post.create(author_id: 1, body: 'This post was seeded into the database.', content:'Random stuff', title: 'Test post')
+
+Category.create(name: 'Gardening', short_name: 'gard', description:'Posts about gardening')
+Category.create(name: 'Music', short_name: 'mus', description:'Posts about Music')
+Category.create(name: 'Family life', short_name: 'fam', description:'Posts about family life')
